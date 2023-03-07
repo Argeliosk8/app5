cd18de751d893658e8dc7b09aa5be05f5a6fc70b
Argeliosk8 <argelio.baca@gmail.com>
2023-03-07 09:22:35 -0600

    UI changes

--- routes.py
+++ routes.py
@@ -5,3 +5,15 @@ from flask import Flask, render_template, url_for, request
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/add_vehicle')
+def add_vehicle():
+    return render_template('add_vehicle.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
